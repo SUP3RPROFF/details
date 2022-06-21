65c2688a904019407094deaf5819a5e428a4f72a
SUP3RPROFF <Aliyuibrahim1088@gmail.com>
2022-06-21 13:17:04 +0100

    summary
    normal

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const name =   prompt('enter your name');
+const height =   prompt('enter height');
+const country =  prompt('enter your country');
+const result = `my name is ${name}, my height is ${height}, and i\'m from ${country}`
+alert(result)
+
+
+        
+    
+
